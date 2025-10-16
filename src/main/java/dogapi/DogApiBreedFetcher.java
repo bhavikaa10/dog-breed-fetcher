e0a8478832d39530c658d2babd114ec146062f94
bhavikaa Goenka <bhavikaa.goenka@mail.utoronto.ca>
2025-10-16 13:43:07 -0400

    Implement CachingBreedFetcher functionality

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,7 +24,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         String url = "https://dog.ceo/api/breed/" + breed + "/list";
         Request request = new Request.Builder().url(url).build();
 
@@ -49,7 +49,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
             return subBreeds;
 
-        } catch (IOException e) {
+        } catch (IOException | BreedNotFoundException e) {
             throw new BreedNotFoundException("Error fetching data from API for: " + breed);
         }
     }
